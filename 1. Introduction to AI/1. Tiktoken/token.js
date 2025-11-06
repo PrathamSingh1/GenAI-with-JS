e34f397799f21f844d59c71a00a7a0dacf43585e
PrathamSingh1 <prathamjii36@gmail.com>
2025-11-06 11:22:37 +0000

    Tiktoken

---
+++ 1. Introduction to AI/1. Tiktoken/token.js
@@ -0,0 +1,19 @@
+import { Tiktoken } from "js-tiktoken/lite";
+import o200k_base from "js-tiktoken/ranks/o200k_base";
+
+
+const enc = new Tiktoken(o200k_base);
+
+
+const userQuery = "Hey There, I am Pratham Singh";
+const tokens = enc.encode(userQuery);
+
+console.log({ tokens });
+console.log("Hello");
+
+
+const inputTokens = [25216, 3274, 11, 357, 939, 2284, 94125, 44807];
+
+const decoded =  enc.decode(inputTokens);
+
+console.log({ decoded });
